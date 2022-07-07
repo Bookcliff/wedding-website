ce1d7924ec949b56f7349965179a05d65f0bb974
Bookcliff <bookcliff76@gmail.com>
2022-07-07 15:17:36 -0500

    vercel issue

--- my-app/api/guests/[id].js
+++ my-app/api/guests/[id].js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from "../my-app/lib/connectToDatabase";
+import { connectToDatabase } from "../../lib/connectToDatabase";
 const { ObjectId } = require("mongodb");
 
 //This function is to populate the RSVP form w/guest names, partner, and RSVP status. Combine into one function (if... elseif)
@@ -23,7 +23,7 @@ export default async function guestDataApi(req, res) {
         $set: {
           rsvp: req.body.rsvp,
           rsvpStatus: true,
-          chipotleOrder: req.body.order,
+          // chipotleOrder: req.body.order,
         },
       }
     );
@@ -40,5 +40,3 @@ export default async function guestDataApi(req, res) {
     res.status(405).json({ status: "ERROR: ROUTE NOT FOUND" });
   }
 }
-
-//Vision: Press RSVP button - hidden container opens with form to add name and check y/n to rsvp (will add in partner function later). Once click submit button, POST req must be sent to db with the name and adding rsvp boolean.
--- my-app/lib/connectToDatabase.js
+++ my-app/lib/connectToDatabase.js
@@ -1,24 +1,24 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 
 let cachedDb = null;
 
 export const connectToDatabase = async () => {
-    if (cachedDb) {
-        console.log('Use exisiting connection');
-        return Promise.resolve(cachedDb);
-    }
-    else {
-        return MongoClient.connect(process.env.MONGODB_URI, {
-            useUnifiedTopology: true,
-        })
-            .then((client) => {
-                let db = client.db('weddingWebsite');
-                console.log('New Database connection');
-                cachedDb = db;
-                return cachedDb;
-            }).catch((error) => {
-                console.log('Mongo Connection error');
-                console.log(error);
-            })
-    }
-}
+  if (cachedDb) {
+    console.log("Use exisiting connection");
+    return Promise.resolve(cachedDb);
+  } else {
+    return MongoClient.connect(process.env.MONGODB_URI, {
+      useUnifiedTopology: true,
+    })
+      .then((client) => {
+        let db = client.db("weddingWebsite");
+        console.log("New Database connection");
+        cachedDb = db;
+        return cachedDb;
+      })
+      .catch((error) => {
+        console.log("Mongo Connection error");
+        console.log(error);
+      });
+  }
+};
